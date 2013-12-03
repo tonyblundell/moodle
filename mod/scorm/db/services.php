aa2a2d58506021a4bc6cb1565243bb59fccdd76f
Tony Blundell <tony.blundell@kineo.com>
2013-12-03 18:54:14 +0000

    MDL-43136 Web service for SCORM details

---
+++ mod/scorm/db/services.php
@@ -0,0 +1,11 @@
+<?php
+
+$functions = array(
+    'mod_scorm_get_attempt_status' => array(
+        'classname'     => 'mod_scorm_external',
+        'methodname'    => 'get_attempt_status',
+        'classpath'     => 'mod/scorm/externallib.php',
+        'description'   => 'Returns the attempt status info for the current user',
+        'type'          => 'read'
+    )
+);
